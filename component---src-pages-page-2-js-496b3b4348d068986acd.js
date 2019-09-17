b98c38e08f755880d695f1acbe5b6f1e1ec2ce84
Leo <leo@studioface.si>
2019-09-17 10:54:23 +0200

    Updates

---
+++ component---src-pages-page-2-js-496b3b4348d068986acd.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{209:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(102),c=a(206),r=a(205);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-496b3b4348d068986acd.js.map
