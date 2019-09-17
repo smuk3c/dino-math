dd3f40240b2433cca77c359e5d23f27df661e906
Leo <leo@studioface.si>
2019-09-17 11:05:30 +0200

    Updating js code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Dino Math`,
+    description: `Simple game for kids, encouraging them to learn math.`,
+    author: `@smuk3c`,
   },
+  pathPrefix: "/dino-math",
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
